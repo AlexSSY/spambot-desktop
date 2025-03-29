cfa30b4ca887871fd5d0d3ca1c7f89befe47b6e5
DXatom <adolf5341@gmail.com>
2025-03-29 21:04:31 +0200

    add tailwindcss

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+from flask import Flask, send_from_directory
 
 import db
 
 
-async def main():
-    await db.setup_db()
-    print("hello world!")
+app = Flask(__name__, static_folder="./web/dist")
 
+@app.route('/')
+async def serve_vue():
+    await db.setup_db()
+    return send_from_directory(app.static_folder, 'index.html')
 
 async def add_phone(phone):
     # check if phone exists in db
     if await db.phone_exists(phone):
         pass
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
